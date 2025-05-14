3dac7203a66dae5cf891e1c9eefb1f74bdc6fc58
Fahad Rasheed <fahadrasheed304@gmail.com>
2025-05-15 02:45:01 +0500

    Added Signup Form #2 and #3

---
+++ bank/management/system/java/SignUpForm2.java
@@ -0,0 +1,164 @@
+
+package bank.management.system.java;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+
+
+public class SignUpForm2 extends JFrame{
+    JLabel headingTitle, additionalDetails, religion, Income, Education, qualification, panCode, ExistAccount, SC, CnicNumber, occupation, category;
+    JTextField CnicTextField, panCodeTextField;
+    JRadioButton sYes, sNo, eYes, eNo;
+    JComboBox Religion, Category, Occupation, income, Qualification, education;
+    long formNO;
+    
+    SignUpForm2(long formNo) {
+        this.formNO = formNo;
+        setLayout(null);
+        setSize(800, 750); 
+        setLocationRelativeTo(null);
+        
+        setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 2");
+        
+        headingTitle = new JLabel("APPLICATION FORM NO. " + formNo);
+        headingTitle.setFont(new Font("Times New Roman", Font.BOLD, 38));
+        headingTitle.setBounds(140, 20, 600, 40);
+        add(headingTitle);
+
+        additionalDetails = new JLabel("PAGE 2: Additional Details ");
+        additionalDetails.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        additionalDetails.setBounds(290, 80, 400, 30);
+        add(additionalDetails);
+
+        religion = new JLabel("Religion: ");
+        religion.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        religion.setBounds(100, 140, 100, 30);
+        add(religion);
+
+        String valReligion[] = {"Null", "Hindu", "Muslim", "Sikh", "Christian", "Other"};
+        Religion = new JComboBox(valReligion);
+        Religion.setBounds(300, 140, 400, 30);
+        Religion.setBackground(Color.WHITE);
+        add(Religion);
+
+        category = new JLabel("Category: ");
+        category.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        category.setBounds(100, 190, 200, 30);
+        add(category);
+
+        String valCategory[] = {"Null", "General", "OBC", "SC", "ST", "Other"};
+        Category = new JComboBox(valCategory);
+        Category.setBounds(300, 190, 400, 30);
+        Category.setBackground(Color.WHITE);
+        add(Category);
+
+        Income = new JLabel("Income: ");
+        Income.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        Income.setBounds(100, 240, 200, 30);
+        add(Income);
+
+        String valIncome[] = {"Null","<1,50,000", "<2,50,000", "<5,00,000", "Upto 10,00,000"};
+        income = new JComboBox(valIncome);
+        income.setBounds(302, 243, 398, 30);
+        income.setBackground(Color.WHITE);
+        add(income);
+
+        Education = new JLabel("Educational: ");
+        Education.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        Education.setBounds(100, 290, 200, 30);
+        add(Education);
+
+        qualification = new JLabel("Qualification: ");
+        qualification.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        qualification.setBounds(100, 315, 200, 30);
+        add(qualification);
+
+        String valQualification[] = {"Null","Graduate","Under Graduate", "Post Graduate", "Doctorate", "Others"};
+        Qualification = new JComboBox(valQualification);
+        Qualification.setBounds(300, 310, 398, 30);
+        Qualification.setBackground(Color.WHITE);
+        add(Qualification);
+
+        occupation = new JLabel("Occupation: ");
+        occupation.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        occupation.setBounds(100, 390, 200, 30);
+        add(occupation);
+
+        String valOccupation[] = {"Null", "Salaried","Self-Employed","Business", "Student", "Retired", "Others"};
+        Occupation = new JComboBox(valOccupation);
+        Occupation.setBounds(302, 390, 398, 30);
+        Occupation.setBackground(Color.WHITE);
+        add(Occupation);
+
+        CnicNumber = new JLabel("CNIC-Number: ");
+        CnicNumber.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        CnicNumber.setBounds(100, 490, 200, 30);
+        add(CnicNumber);
+
+        CnicTextField = new JTextField();
+        CnicTextField.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        CnicTextField.setBounds(300, 490, 400, 30);
+        add(CnicTextField);
+
+        SC = new JLabel("Senior Citizen: ");
+        SC.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        SC.setBounds(100, 540, 200, 30);
+        add(SC);
+
+        sYes = new JRadioButton("Yes");
+        sYes.setBounds(300, 540, 60, 30);
+        sYes.setBackground(Color.WHITE);
+        add(sYes);
+
+        sNo = new JRadioButton("NO");
+        sNo.setBounds(450, 540, 70, 30);
+        sNo.setBackground(Color.WHITE);
+        add(sNo);
+
+        ButtonGroup SeniorGroup = new ButtonGroup();
+        SeniorGroup.add(sYes);
+        SeniorGroup.add(sNo);
+
+        ExistAccount = new JLabel("Existing Account: ");
+        ExistAccount.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        ExistAccount.setBounds(100, 590, 200, 30);
+        add(ExistAccount);
+
+        eYes = new JRadioButton("Yes");
+        eYes.setBounds(300, 590, 60, 30);
+        eYes.setBackground(Color.WHITE);
+        add(eYes);
+
+        eNo = new JRadioButton("No");
+        eNo.setBounds(450, 590, 70, 30);
+        eNo.setBackground(Color.WHITE);
+        add(eNo);
+
+        ButtonGroup ExistingGroup = new ButtonGroup();
+        ExistingGroup.add(eYes);
+        ExistingGroup.add(eNo);
+
+        panCode = new JLabel("PAN Number: ");
+        panCode.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        panCode.setBounds(100, 439, 200, 30);
+        add(panCode);
+
+        panCodeTextField = new JTextField();
+        panCodeTextField.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        panCodeTextField.setBounds(300, 439, 400, 30);
+        add(panCodeTextField);
+
+        JButton next = new JButton("Next");
+        next.setBackground(Color.GRAY); 
+        next.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        next.setBounds(620, 660, 80, 30);
+        add(next);
+
+        getContentPane().setBackground(new Color(240, 240, 240));
+
+
+        setVisible(true);
+    }
+}
---
+++ bank/management/system/java/SignUpForm3.java
@@ -0,0 +1,151 @@
+
+package bank.management.system.java;
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class SignUpForm3 extends JFrame{
+    JLabel i1, type, card, number, pin, pnumber, cardDetails, pinDetails, services, headingTitle;
+    JRadioButton r1, r2, r3, r4;
+    JCheckBox c1, c2, c3, c4, c5, c6, c7;
+    JButton cancel, submit;
+    long formNo;
+    
+    SignUpForm3(long formNo) {
+        this.formNo = formNo;
+
+        setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 3");
+        setLayout(null);
+        setSize(800, 750); // Set a preferred size for the window
+        setLocationRelativeTo(null);
+        
+        headingTitle = new JLabel("APPLICATION FORM NO. " + formNo);
+        headingTitle.setFont(new Font("Times New Roman", Font.BOLD, 38));
+        headingTitle.setBounds(140, 10, 600, 40);
+        add(headingTitle);
+
+        cardDetails = new JLabel("Page 3: Account Details");
+        cardDetails.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        cardDetails.setBounds(290, 70, 400, 30);
+        add(cardDetails);
+
+        type = new JLabel("Account Type: ");
+        type.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        type.setBounds(100, 120, 200, 30);
+        add(type);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        r1.setBounds(300, 120, 250, 30);
+        r1.setBackground(Color.WHITE);
+        add(r1);
+
+        r2 = new JRadioButton("Fixed Deposit Account");
+        r2.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        r2.setBounds(300, 160, 250, 30);
+        r2.setBackground(Color.WHITE);
+        add(r2);
+
+        r3 = new JRadioButton("Current Account");
+        r3.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        r3.setBounds(300, 200, 250, 30);
+        r3.setBackground(Color.WHITE);
+        add(r3);
+
+        r4 = new JRadioButton("Recurring Deposit Account");
+        r4.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        r4.setBounds(300, 240, 300, 30);
+        r4.setBackground(Color.WHITE);
+        add(r4);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(r1);
+        group.add(r2);
+        group.add(r3);
+        group.add(r4);
+
+        card = new JLabel("Card Number: ");
+        card.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        card.setBounds(100, 290, 200, 30);
+        add(card);
+
+        number = new JLabel("XXXX-XXXX-XXXX-XXXX");
+        number.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        number.setBounds(300, 290, 250, 30);
+        add(number);
+
+        pin = new JLabel("PIN: ");
+        pin.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        pin.setBounds(100, 340, 200, 30);
+        add(pin);
+
+        pnumber = new JLabel("XXXX");
+        pnumber.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        pnumber.setBounds(300, 340, 250, 30);
+        add(pnumber);
+
+        services = new JLabel("Services Required: ");
+        services.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        services.setBounds(100, 390, 250, 30);
+        add(services);
+
+        c1 = new JCheckBox("ATM Card");
+        c1.setBackground(Color.WHITE);
+        c1.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c1.setBounds(300, 390, 150, 30);
+        add(c1);
+
+        c2 = new JCheckBox("Internet Banking");
+        c2.setBackground(Color.WHITE);
+        c2.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c2.setBounds(300, 430, 150, 30);
+        add(c2);
+
+        c3 = new JCheckBox("Mobile Banking");
+        c3.setBackground(Color.WHITE);
+        c3.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c3.setBounds(300, 470, 150, 30);
+        add(c3);
+
+        c4 = new JCheckBox("Email Alerts");
+        c4.setBackground(Color.WHITE);
+        c4.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c4.setBounds(300, 510, 150, 30);
+        add(c4);
+
+        c5 = new JCheckBox("Cheque Book");
+        c5.setBackground(Color.WHITE);
+        c5.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c5.setBounds(300, 550, 150, 30);
+        add(c5);
+
+        c6 = new JCheckBox("E-Statements");
+        c6.setBackground(Color.WHITE);
+        c6.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c6.setBounds(300, 590, 150, 30);
+        add(c6);
+
+        c7 = new JCheckBox("None");
+        c7.setBackground(Color.WHITE);
+        c7.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        c7.setBounds(300, 630, 150, 30);
+        add(c7);
+
+        cancel = new JButton("Cancel");
+        cancel.setBounds(300, 670, 100, 30);
+        cancel.setBackground(Color.GRAY);
+        cancel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        add(cancel);
+
+        submit = new JButton("Submit");
+        submit.setBounds(420, 670, 100, 30);
+        submit.setBackground(Color.GRAY);
+        submit.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        add(submit);
+
+        getContentPane().setBackground(new Color(240, 240, 240));
+
+        setVisible(true);
+    }
+}
