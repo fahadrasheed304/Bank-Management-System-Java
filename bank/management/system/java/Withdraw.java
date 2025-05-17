e55f022ca0ea1c3bc91cec0bdd99a62d6372f10b
Rafay Khan <118996087+rafaykhan122@users.noreply.github.com>
2025-05-17 19:12:14 +0500

    Update Withdraw.java
    Added ActionListener

--- bank/management/system/java/Withdraw.java
+++ bank/management/system/java/Withdraw.java
@@ -1,22 +1,18 @@
-package bank.management.system.java;
-/**
- *
- * @author Fahad Rasheed
- */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
 
+public class Withdraw extends JFrame implements ActionListener{
 
-public class Withdraw extends JFrame{ 
     ImageIcon i1, i3;
     Image i2;
     JLabel image, text;
     JButton back, withdraw;
     JTextField amount;
-    String pinNumber, accountNo;
-    Withdraw(String pinNumber, String accountNo){
+    String pinNumber;
+    Withdraw(String pinNumber){
         this.pinNumber = pinNumber;
-        this.accountNo = accountNo;
         setLayout(null);
 
         i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
@@ -39,10 +35,12 @@ public class Withdraw extends JFrame{
 
         withdraw = new JButton("Withdraw");
         withdraw.setBounds(355, 485, 150, 30);
+        withdraw.addActionListener(this);
         image.add(withdraw);
 
         back = new JButton("Back");
         back.setBounds(355, 520, 150, 30);
+        back.addActionListener(this);
         image.add(back);
 
         setSize(900,900);
@@ -50,7 +48,33 @@ public class Withdraw extends JFrame{
         setUndecorated(true);
         setVisible(true);
     }
-  public static void main(String[] args) {
-        new Withdraw("", "").setVisible(true);
+
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource() == withdraw){
+            String number = amount.getText();
+            Date date = new Date();
+            if(number.equals("")){
+                JOptionPane.showMessageDialog(null, "Please enter the amount you want to withdraw");
+            }else {
+                try {
+                    Conn conn = new Conn();
+                    String query = "insert into bank (pin_number, transaction_type, transaction_date, amount) VALUES (?, ?, ?, ?)";
+                    conn.s.executeUpdate(query);
+                    JOptionPane.showMessageDialog(null, "Rs" + number + "Withdraw Successfully");
+                    setVisible(false);
+                    new Transactions( pinNumber).setVisible(true);
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        }else if(ae.getSource() == back){
+            setVisible(false);
+            new Transactions(pinNumber).setVisible(true);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new Withdraw("");
     }
 }
